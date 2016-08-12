5c79671c44cfa549b585d4604914a1375c6d7123
Jos Lagerweij <j.lagerweij@avisi.nl>
2016-08-12 13:19:13 +0200

    Add intro with hello world, java project and java project with integration test

---
+++ demo/intro/src/integration-test/java/PersonIntegrationTest.java
@@ -0,0 +1,9 @@
+import org.custommonkey.xmlunit.XMLUnit;
+import org.junit.Test;
+
+public class PersonIntegrationTest {
+    @Test
+    public void testSomething() {
+        XMLUnit xmlUnit;
+    }
+}
---
+++ demo/intro/src/main/java/Person.java
@@ -0,0 +1,11 @@
+import org.apache.commons.collections.list.GrowthList;
+
+public class Person {
+
+    private Integer number;
+
+    public static void main(String[] args) {
+        GrowthList list = new GrowthList();
+        System.out.println("Hello world from the Person class");
+    }
+}
